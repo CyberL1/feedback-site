9e2d7f9e6b93242e2aa7772b39f6e0eab23c76cb
Cyber <44707958+CyberLinx@users.noreply.github.com>
2020-05-03 18:01:30 +0200

    Update upvoted.php

--- user/suggestions/upvoted.php
+++ user/suggestions/upvoted.php
@@ -31,8 +31,8 @@ if ($upvotedquery = mysqli_query ($connect, "SELECT * FROM `suggestions` ORDER B
     $login = $_SESSION['login'];
     $votecheck = mysqli_query($connect, "SELECT * FROM `voters` WHERE `vote_nr`='$nr' AND `voter`='$login'");
     if(mysqli_num_rows($votecheck)) {
-        if(!$upvoted['edited']) echo "<a href='index.php?show=$nr'><p class='text-left'><b>".substr(strip_tags($upvoted['title']), 0, 32)."</a></b></p><p cliass='text-right'><i>".strip_tags($upvoted['date'])."</i> Author: <b><a href='users.php?nick=$nick'>".substr(strip_tags($upvoted['nick']), 0, 22)."</a></b></i> | Votes: <b>".$upvoted['votes']."</b></p><p class='text-right'>Status: <b>".$upvoted['status']."</b></p>\n<hr>";
-        if($upvoted['edited']) echo "<a href='index.php?show=$nr'><p class='text-left'><b>".substr(strip_tags($upvoted['title']), 0, 32)."</a></b></p><p cliass='text-right'><i>".strip_tags($upvoted['date'])."</i> Author: <b><a href='users.php?nick=$nick'>".substr(strip_tags($upvoted['nick']), 0, 22)."</a></b></i> | Votes: <b>".$upvoted['votes']."</b></p><p class='text-right'>Status: <b>".$upvoted['status']."</b> | Edited on: <b>".$upvoted['edit_date']."</b></p>\n<hr>";
+        if(!$upvoted['edited']) echo "<a href='/index.php?show=$nr'><p class='text-left'><b>".substr(strip_tags($upvoted['title']), 0, 32)."</a></b></p><p cliass='text-right'><i>".strip_tags($upvoted['date'])."</i> Author: <b><a href='users.php?nick=$nick'>".substr(strip_tags($upvoted['nick']), 0, 22)."</a></b></i> | Votes: <b>".$upvoted['votes']."</b></p><p class='text-right'>Status: <b>".$upvoted['status']."</b></p>\n<hr>";
+        if($upvoted['edited']) echo "<a href='/index.php?show=$nr'><p class='text-left'><b>".substr(strip_tags($upvoted['title']), 0, 32)."</a></b></p><p cliass='text-right'><i>".strip_tags($upvoted['date'])."</i> Author: <b><a href='users.php?nick=$nick'>".substr(strip_tags($upvoted['nick']), 0, 22)."</a></b></i> | Votes: <b>".$upvoted['votes']."</b></p><p class='text-right'>Status: <b>".$upvoted['status']."</b> | Edited on: <b>".$upvoted['edit_date']."</b></p>\n<hr>";
     }
 }
 }
